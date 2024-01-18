d5d7b83cad214dc977676355bd4a8e23ac177180
Omri Barouch <omby8888@gmail.com>
2024-01-18 16:22:23 +0200

    Initial Commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { MoviesPageComponent } from "./components/movies-page/movies-page.component";
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: MoviesPageComponent
+  }
+];
